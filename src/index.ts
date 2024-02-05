092fb8cff988615be85f837aedeb3def002e61b4
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-02-05 21:08:03 +0530

    crud

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,64 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+interface User  {
+    email: string
+    password: string
+    name?: string
+
+}
+
+async function createUser({email, password, name}:User) {
+    const response = await prisma.user.create({
+        data: {
+            email: email,
+            password: password,
+            name: name
+        }
+    })
+    console.log(response);
+}
+
+// createUser({
+//     email: "s@s.a",
+//     password: "12345"
+// })
 
 
+interface UpdateUser {
+    password: string
+    name: string
+}
+
+async function updateUser(email: string, {
+    password,
+    name
+}: UpdateUser) {
+    const res = await prisma.user.update({
+        data: {
+            name: name,
+            password: password
+        },
+        where: {
+            id: 1
+        }
+    })
+    console.log(res);
+}
+
+// updateUser("s@s.a", {
+//     password: "s1234",
+//     name: "sahil"
+// })
+
+
+async function getUser(username: string) {
+    const res = await prisma.user.findFirst({
+        where: {
+            email:username   
+        }
+    })
+    console.log(res);
+}
+
+getUser("s@s.a")
